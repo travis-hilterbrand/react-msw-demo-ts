b1aea21aea8902e2008386516fb5f72b96062186
Travis Hilterbrand <travis.hilterbrand@zingly.ai>
2024-07-08 00:10:34 -0700

    Disable refetch on focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Counter, UsersView } from "./Users";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 function App() {
   return (
